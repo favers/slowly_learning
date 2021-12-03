ca33bf5628a41319c2da91385486bae86919e728
favers <favers0601@gmail.com>
2021-12-03 17:08:54 +0800

    feat: 格式化

--- rust_by_example/hello/src/main.rs
+++ rust_by_example/hello/src/main.rs
@@ -16,7 +16,8 @@ fn main() {
     // display_print();
     // display_min_max();
     // display_complex();
-    display_list();
+    // display_list();
+    display_format();
 }
 
 fn comments() {
@@ -220,4 +221,99 @@ fn display_list() {
     }
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
+}
+
+fn display_format() {
+    struct City {
+        name: &'static str,
+        // 纬度
+        lat: f32,
+        // 经度
+        lon: f32,
+    }
+    impl fmt::Display for City {
+        // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+            // 一个缓冲区（即第一个参数 f）中。
+            write!(
+                f,
+                "{}: {:.3}°{} {:.3}°{}",
+                self.name,
+                self.lat.abs(),
+                lat_c,
+                self.lon.abs(),
+                lon_c
+            )
+        }
+    }
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    impl fmt::Display for Color {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let r = self.red;
+            let g = self.green;
+            let b = self.blue;
+            write!(
+                f,
+                "RGB ({r},{g},{b}) {:#02X}{:02X}{:02X}",
+                r = r,
+                g = g,
+                b = b
+            )
+        }
+    }
+
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{}", *color)
+    }
 }
